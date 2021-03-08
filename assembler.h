f0b7b8fea2ddc850e3e7a06fce610e9b0bc108fa
Jinxiang-Lin <78502763+Jinxiang-Lin@users.noreply.github.com>
2021-03-08 15:48:15 -0500

    Update assembler.h

--- assembler.h
+++ assembler.h
@@ -11,6 +11,10 @@ public:
 	void inputSource(const vector<string> newSource);
 
 	void printSource() const;
+
+/* source vector stores the original c++ code;
+   assembly_code stores the assembly code which is translated from source;
+   address offset defaults to 0*/
 private:
 	vector<string> source = "";
 	vector<string> assembly_codes = "";
