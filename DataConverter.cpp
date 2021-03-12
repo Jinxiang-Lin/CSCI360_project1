ea78334934828310f2e947b6cc1fa309d69d10ea
LolsMeow <jzheng1818@bths.edu>
2021-03-12 18:54:04 -0500

    Completed Arithmetic_Handler
    Added add_mov to add movq or movl to the assembly instruction, will need it more for the function_handler later.
    Added a get_offset to get the addr_offset of a variable. Did not code it because need to see the variable handler for it to work.

---
+++ DataConverter.cpp
@@ -0,0 +1,19 @@
+#include "DataConverter.h"
+
+/*
+	Function: split string by character
+*/
+void DataConverter::split(const string &txt, vector<string> &strs, char ch)
+{
+    size_t pos = txt.find( ch );
+    size_t initialPos = 0;
+    strs.clear();
+    // Decompose statement
+    while( pos != std::string::npos ) {
+        strs.push_back( txt.substr( initialPos, pos - initialPos ) );
+        initialPos = pos + 1;
+        pos = txt.find( ch, initialPos );
+    }
+    // Add the last one
+    strs.push_back( txt.substr( initialPos, std::min( pos, txt.size() ) - initialPos + 1 ) );
+}
